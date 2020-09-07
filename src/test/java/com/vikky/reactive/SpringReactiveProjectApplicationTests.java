9e5d86a93db4f5229c537d557d065a7bd72a6650
vikky kumar <loveuvikky2@gmail.com>
2020-09-08 02:41:52 +0530

    Initial commit

---
+++ src/test/java/com/vikky/reactive/SpringReactiveProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikky.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
