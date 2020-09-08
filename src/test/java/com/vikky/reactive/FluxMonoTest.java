ed3bde1bef3e2267ed8ca68540e97d66fcbe987e
vikky kumar <loveuvikky2@gmail.com>
2020-09-08 18:38:30 +0530

    Added the first test

---
+++ src/test/java/com/vikky/reactive/FluxMonoTest.java
@@ -0,0 +1,34 @@
+package com.vikky.reactive;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class FluxMonoTest {
+
+  @Test
+  public void fluxTest() {
+    Flux<String> stringFlux =
+        Flux.just("Spring boot", "Spring Data", "Spring Security")
+            .concatWith(Flux.error(new RuntimeException("I dont know but something went wrong")))
+            .log();
+
+    stringFlux.subscribe(i -> System.out.println(i),
+            e -> System.err.println(e));
+
+  }
+  @Test
+  public void testFluxWithError(){
+    Flux<String> stringFlux =
+            Flux.just("Spring boot", "Spring Data", "Spring Security")
+                    .concatWith(Flux.error(new RuntimeException("I dont know but something went wrong")))
+                    .log();
+
+    StepVerifier.create(stringFlux)
+            .expectNext("Spring boot")
+            .expectNext("Spring Data")
+            .expectNext("Spring Security")
+            .expectErrorMessage("I dont know but something went rong");
+            //.verifyComplete();
+  }
+}
