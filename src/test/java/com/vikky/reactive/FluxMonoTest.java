9a01fbc7041c13e7d477df869bcf42c3841f1b20
vikky kumar <loveuvikky2@gmail.com>
2020-09-08 19:44:55 +0530

    Updated

--- src/test/java/com/vikky/reactive/FluxMonoTest.java
+++ src/test/java/com/vikky/reactive/FluxMonoTest.java
@@ -13,22 +13,22 @@ public class FluxMonoTest {
             .concatWith(Flux.error(new RuntimeException("I dont know but something went wrong")))
             .log();
 
-    stringFlux.subscribe(i -> System.out.println(i),
-            e -> System.err.println(e));
-
+    stringFlux.subscribe(i -> System.out.println(i), e -> System.err.println(e));
   }
+
   @Test
-  public void testFluxWithError(){
+  public void testFluxWithError() {
     Flux<String> stringFlux =
-            Flux.just("Spring boot", "Spring Data", "Spring Security")
-                    .concatWith(Flux.error(new RuntimeException("I dont know but something went wrong")))
-                    .log();
+        Flux.just("Spring boot", "Spring Data", "Spring Security")
+            .concatWith(Flux.error(new RuntimeException("I dont know but something went wrong")))
+            .log();
 
     StepVerifier.create(stringFlux)
-            .expectNext("Spring boot")
-            .expectNext("Spring Data")
-            .expectNext("Spring Security")
-            .expectErrorMessage("I dont know but something went rong");
-            //.verifyComplete();
+        .expectNext("Spring boot")
+        .expectNext("Spring Data")
+        .expectNext("Spring Security")
+            .expectError(RuntimeException.class);
+        //.expectErrorMessage("I dont know but something went rong");
+    // .verifyComplete();
   }
 }
